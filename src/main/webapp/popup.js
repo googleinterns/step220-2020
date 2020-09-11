9f9a4cb859442d303a08566b88a8a00d186071a3
RemusNeatu <44118877+RemusNeatu@users.noreply.github.com>
2020-09-11 13:47:05 +0300

    Functionality for popup window (#13)
    * Create fields for popup window
    
    * Add hyphens in class names. Change CSS style file in a more specified way
    
    * Add functionality for popup window (appear and disappear)
    
    * Add TODO commentary

---
+++ src/main/webapp/popup.js
@@ -0,0 +1,15 @@
+// TODO(remusn@) Create a class that can manage multiple popup objects
+
+/**
+ * Opens a popup window for adding an event when the add-button is pressed
+ */
+function openEventPopup() {
+    document.getElementById('add-event').style.display = 'block';
+}
+
+/**
+ * Closes the popup window by pressing the "Cancel" button inside that popup
+ */
+function closeEventPopup() {
+    document.getElementById('add-event').style.display = 'none';
+}
