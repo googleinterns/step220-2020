60aabbe570e2977e0aa16342dbad6aca3f39ebc8
RemusNeatu <neaturemus07@gmail.com>
2020-09-17 09:27:52 +0000

    Add functionality for submit button

---
+++ src/main/webapp/Event.js
@@ -0,0 +1,11 @@
+/**
+ * Events format class
+ */
+export default class Event {
+    constructor(title, location, startingTime, endingTime) {
+        this.title = title;        
+        this.location = location;        
+        this.startingTime = startingTime;        
+        this.endingTime = endingTime;        
+    }
+}
--- src/main/webapp/popup.js
+++ src/main/webapp/popup.js
@@ -1,3 +1,5 @@
+import Event from './Event.js';
+
 // TODO(remusn@) Create a class that can manage multiple popup objects
 
 /**
@@ -12,4 +14,32 @@ function openEventPopup() {
  */
 function closeEventPopup() {
     document.getElementById('add-event').style.display = 'none';
+}
+
+/**
+ * Creates a new Event object and add it to the event list
+ */
+function createNewEvent() {
+    const title = document.getElementById('title').value;
+    const location = document.getElementById('location').value;
+    const startingTime = document.getElementById('starting-time').value;
+    const endingTime = document.getElementById('ending-time').value;
+
+    // Proceed only if all the fields are completed
+    if (!title || !location || !startingTime || !endingTime) {
+        return;
+    }
+
+    const event = new Event(title, location, startingTime, endingTime);
+    addEvent(event);
+}
+
+// Export functions to global object "window"
+window.openEventPopup = openEventPopup;
+window.closeEventPopup = closeEventPopup;
+window.createNewEvent = createNewEvent;
+
+// TODO (remusn@)
+function addEvent(event) {
+
 }
