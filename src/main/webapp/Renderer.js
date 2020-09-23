937d9ac3b4cf3583c88cb75a40a5d9f0d1130b36
Nick Tzavidas <tzavidas@google.com>
2020-09-23 13:27:03 +0000

    Updated the style and of step1 (+popup)

--- src/main/webapp/Renderer.js
+++ src/main/webapp/Renderer.js
@@ -9,12 +9,17 @@ export default class Renderer {
     render(events) {
         this.eventList.innerHTML = "";
 
-        for (let event of events) {
+        for (let [ index, event ] of events.entries()) {
             this.eventList.innerHTML += `
-                <div class="event-container">
-                    <h3>${event.title}</h3>
-                    <p>${event.startTime} - ${event.endTime}</p>
-                    <p>${event.location}</p>
+                <div class="event-wrapper">
+                    <div class="event-details">
+                        <h3>${event.title}</h3>
+                        <p>${event.startTime} - ${event.endTime}</p>
+                        <p>${event.location}</p>
+                    </div>
+                    <div class="remove-button-container">
+                        <button onclick="window.deleteEvent(${index})">+</button>
+                    </div>
                 </div>
             `
         }
